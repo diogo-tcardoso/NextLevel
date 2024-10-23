351b0f2912eb48a99960a35836154b0982937fe4
diogo.tcardoso <diocardoso@hotmail.com>
2024-10-23 13:52:14 -0300

    Removing Google Tag Manager - Google Analytics

--- src/js/gtag.js
+++ src/js/gtag.js
@@ -3,9 +3,3 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'AW-16717290137');
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-08L2F0PT5X');
